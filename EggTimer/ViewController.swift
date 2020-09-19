90ae26253550b3a50dc59d981e05387a79fd0975
Nikita <nikitalalwani93@gmail.com>
2020-09-18 18:55:11 -0700

    First commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(softTime)
+        } else if hardness == "Medium" {
+            print(mediumTime)
+        } else {
+            print(hardTime)
+        }
+        
+    }
+    
+}
